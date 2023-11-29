6caad022824f0d6e06f5af5b1cbf6b80eddf3114
Alvaro <alvarodr_97@hotmail.com>
2023-11-29 20:34:00 +0100

    styling, useDB, assets

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,8 @@
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useUser } from "../hooks/useUser";
+import exit from '../assets/exit.png'
+import user from '../assets/user.png'
+import buy from '../assets/buy.png'
 
 export const Game = () => {
   const location = useLocation();
@@ -13,37 +16,51 @@ export const Game = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { cookies, setCookies, nivel, setNivel, setIsFirstRun } = useUser(userName);
 
-
-
-
-
-  
-
-
   return (
-    <div>
-      <p>Nombre del usuario</p>
-      <p>Nombre: {userName}</p>
-      <p>Cookies: {cookies} </p>
-      <button
-        onClick={() => navigate("/")}
-      >Salir</button>
+    <div className="flex flex-col items-center gap-y-12 w-full ">
+      <div className="flex flex-row gap-x-10 w-full justify-between max-w-sm">
+        <div className="flex flex-row justify-center items-center gap-x-1">
+          <img className="w-4 h-4" title="Usuario" src={user} />
 
-      <button className="bg-green-500" onClick={() => setCookies(cookies => cookies! +1)}>
-        +1
+          <p className="font-bold">{userName}</p>
+        </div>
+        
+        <p>Cookies: <span className="font-semibold">{cookies}</span> </p>
+
+        <p>Nivel: {nivel} </p>
+      </div>
+
+      
+      
+
+      <button className="bg-yellow-400 p-4 rounded-full hover:bg-opacity-70 transition-all" onClick={() => setCookies(cookies => cookies! +1)}>
+        + Cookie
       </button>
 
-      {
-        cookies! >= 50 && (
-          <>
-            {/* <button className="bg-yellow-400" onClick={() => setNivel(nivel => nivel +1) }> */}
-            <button className="bg-yellow-400" onClick={() => { setIsFirstRun(false); setNivel(nivel => nivel +1)} }>
-              Comprar auto-merger ({50 * nivel})
-            </button>
-            <p>Nivel: {nivel} </p>
-          </>
-        )
-      }
+      <div className="flex flex-col gap-y-4">
+        
+        {/* <h2 className="mt-3 mb-2">Mejoras:</h2> */}
+
+        <div>
+        <button title="Comprar" className={`bg-yellow-400 ${cookies! >= (50 * nivel) ? "opacity-100" : "opacity-50"} flex flex-row justify-center items-center px-10 py-2`} disabled={cookies! < (50 * nivel) ? true : false} onClick={() => { setIsFirstRun(false); setNivel(nivel => nivel +1)} }>
+        <img className="w-4 h-4 mr-1" title="Comprar" src={buy} />
+         auto-click ({50 * nivel})
+      </button>
+        </div>
+       
+       
+      </div>
+
+      <div>
+        <button
+          onClick={() => navigate("/")}
+          title="Salir"
+        >
+          <img className="w-7 h-7" alt="Salir" src={exit} />
+        </button>
+      </div>
+
+      
     </div>
   )
 }
