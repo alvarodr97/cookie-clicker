d49904d8dec289009f36dc66a8667fa86d7310d7
Alvaro <alvarodr_97@hotmail.com>
2023-11-30 19:54:21 +0100

    feat: manifest testing

--- src/main.tsx
+++ src/main.tsx
@@ -36,3 +36,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then(() => console.log('Service worker registered'))
+      .catch(error => console.error('Error registering service worker', error));
+  });
+}
