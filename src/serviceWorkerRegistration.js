cccc790b93d105290756f2a44d9ee8701b798ccb
Alvaro <alvarodr_97@hotmail.com>
2023-11-30 21:36:56 +0100

    feat: correcion serviceWorkerRegistration

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -19,7 +19,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${import.meta.env.VITE_PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.env.VITE_PUBLIC_URL}service-worker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
