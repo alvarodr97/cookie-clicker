af5521e002e40a17d1ad109d3ff0b10e03ac86ce
Alvaro <alvarodr_97@hotmail.com>
2023-11-30 21:14:58 +0100

    feat: offline, serviceworkers, index, webrix

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
+// @ts-expect-error: Unreachable code error
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
 import App from "./App.tsx";
 import "./index.css";
 import { Game } from "./pages/Game.tsx";
 import { Home } from "./pages/Home.tsx";
+import Offline from "./components/Offline.tsx";
 
 const router = createBrowserRouter([
   {
@@ -33,14 +36,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Offline>
+      <RouterProvider router={router} />
+    </Offline>
   </React.StrictMode>
 );
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceWorker.js')
-      .then(() => console.log('Service worker registered'))
-      .catch(error => console.error('Error registering service worker', error));
-  });
-}
+registerServiceWorker();
